8ca8cd45d5022450ff09ffe7ab7cfb46549ec001
KihiaM <kihiamuthure@yahoo.com>
2019-10-28 11:51:18 +0300

    add bootstrap links in html

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,12 @@ function birthdate() {
   var year = parseInt(document.getElementById("yy").value);
   var month = parseInt(document.getElementById("mm").value);
   var day = parseInt(document.getElementById("dd").value);
-  var da
   var a = (14 - month) / 12;
   var y = year - a;
   var m = month + 12 * a - 2;
   var d = Math.trunc((day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7);
-  var male = document.getElementById("ml").value;
-  var female = document.getElementById("fl").value;
+  var male = document.getElementById("ml");
+  var female = document.getElementById("fl");
 
   var namesMale = [
     "Monday: Kwadwo",
@@ -26,14 +25,14 @@ function birthdate() {
     "Sunday: Kwasi"
   ];
 
-  var namesFemale = [,
+  var namesFemale = [
     "Monday: Adwoa",
 
     "Tuesday: Abenaa",
 
     "Wednesday: Akua",
 
-    "Thursday:  Yaa",
+    "Thursday: Yaa",
 
     "Friday: Afua",
 
@@ -41,11 +40,13 @@ function birthdate() {
 
     "Sunday: Akosua"
   ];
-
-  if (male.checked = true) {
+  
+  if (male.checked == true) {
+    console.log("male");
     alert(namesMale[d]);
-  } 
-  else if (female.checked = true){
-    alert(namesFemale = [d]);
+  } else if (female.checked == true) {
+    console.log("female");
+    alert(namesFemale[d]);
   }
+  
 }
